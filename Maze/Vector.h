fbb2becf25e6a4b5a879c73c27f1c65481a5d2ba
dpel959 <dpel959@naver.com>
2026-07-25 10:41:08 +0900

    자작 Vector 클래스에 Iterator 추가 구현

--- Maze/Vector.h
+++ Maze/Vector.h
@@ -3,10 +3,62 @@
 
 // array랑 거의 같음. 그냥 동적으로 움직이는 것만 더 있으면 됨.
 
+template<typename T>
+class Iterator
+{
+public:
+	Iterator() : _ptr(nullptr) {}
+	Iterator(T* ptr) : _ptr(ptr) {}
+
+	Iterator& operator++ ()
+	{
+		++_ptr;
+		return *this;
+	}
+
+	Iterator operator++ (int) // 후위
+	{
+		Iterator temp = *this;
+		++_ptr;
+		return temp;
+	}
+
+	Iterator operator + (const int count) const
+	{
+		Iterator temp = *this;
+		temp._ptr += count;
+		return temp;
+	}
+
+	bool operator == (const Iterator& other) const
+	{
+		return _ptr == other._ptr;
+	}
+
+	bool operator != (const Iterator& other) const
+	{
+		return _ptr != other._ptr;
+	}
+
+	T& operator * ()
+	{
+		return *_ptr;
+	}
+public:
+	T* _ptr = nullptr;
+};
+
 template <typename T>
 class Vector
 {
 public:
+	// 굳이 외부 클래스를 Vector 클래스를 통해 이렇게 부르는 거 맞다.
+	// 아니, 대체 왜요? -> 가장 핵심 : 컨테이너::iterator 이 형태를 유지해야 template로 돌려 쓸 거 아니야. auto는 문제 없겠다만
+	// 그리고 만약 클래스 이름을 바꿔도 은닉화해서 사용자가 코드를 안 바꿔도 되니까 + 컨테이너 이름이 iterator이름에 붙어 널려있는 것보다 깔금 뭐 이건 부수적인 것
+	using iterator = Iterator<T>;
+
+	iterator begin() { return iterator(_buffer); }
+	iterator end() { return iterator(_buffer + _size); }
 
 	explicit Vector();
 	explicit Vector(int capacity);
